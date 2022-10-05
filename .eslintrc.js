44f4f2aef9af6ac7ea2ce0de43761833ef28459b
cullyfung <fcx.cli@qq.com>
2022-10-05 20:26:36 +0800

    fix: remove eslint something config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,6 @@ module.exports = {
     'es2021': true,
     'node': true
   },
-  'settings': {
-    'react': {
-      'version': 'detect'
-    }
-  },
   'extends': [
     'eslint:recommended',
     'plugin:react/recommended',
